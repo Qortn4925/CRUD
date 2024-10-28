f6884c19018fedd94f319fa437ffd01d0453f8d3
qortn4925 <qortn4925@naver.com>
2024-10-28 10:30:54 +0900

    검색 시 마지막 페이지 번호 계산을 위한 카운트 올 쿼리 수정

--- src/main/java/com/example/prjjsp20241022/mapper/BoardMapper.java
+++ src/main/java/com/example/prjjsp20241022/mapper/BoardMapper.java
@@ -73,8 +73,21 @@ public interface BoardMapper {
     List<Board> selectAllPaging(Integer offset, String searchTarget, String keyword);
 
     @Select("""
+    <script>
     SELECT  count(*) from board
+                  <trim prefix="WHERE" prefixOverrides="OR">
+                                         <if test="searchTarget == 'all' or searchTarget == 'title'">
+                                             title LIKE CONCAT('%', #{keyword}, '%')
+                                         </if>
+                                         <if test="searchTarget == 'all' or searchTarget == 'content'">
+                                             OR content LIKE CONCAT('%', #{keyword}, '%')
+                                         </if>
+                                         <if test="searchTarget == 'all' or searchTarget == 'writer'">
+                                             OR writer LIKE CONCAT('%', #{keyword}, '%')
+                                         </if>
+                                     </trim>
+</script>
 """)
-    Integer countAll();
+    Integer countAll(String keyword,String searchTarget);
 
 }
